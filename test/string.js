b33729b2d998b2eaf648eb670bd1a1f5c02538af
ajlopez <webmaster@ajlopez.com>
2014-02-20 19:32:56 -0300

    First text match

--- test/string.js
+++ test/string.js
@@ -8,3 +8,13 @@ exports['no result'] = function (test) {
     test.ok(typeof result == 'object');
     test.equal(Object.keys(result).length, 0);
 }
+
+exports['match exact word'] = function (test) {
+    anna.define('foo', { type: 'bar' });
+    var result = anna.analyze('foo');
+    
+    test.ok(result);
+    test.ok(typeof result == 'object');
+    test.equal(Object.keys(result).length, 1);
+    test.equal(result.type, 'bar');
+}
