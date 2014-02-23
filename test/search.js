b4bd7e772a151e8cee182ab796f0b9a5fb82fd2c
ajlopez <webmaster@ajlopez.com>
2014-02-23 11:32:09 -0300

    add and search

--- test/search.js
+++ test/search.js
@@ -1,6 +1,17 @@
 ﻿
 var items = require('../lib/items');
 
+function areEqual(obj1, obj2) {
+    if (Object.keys(obj1).length != Object.keys(obj2).length)
+        return false;
+        
+    for (var n in obj1)
+        if (obj1[n] != obj2[n])
+            return false;
+            
+    return true;
+}
+
 exports['retrieve nothing'] = function (test) {
     var result = items.search('foo');
     
@@ -9,3 +20,12 @@ exports['retrieve nothing'] = function (test) {
     test.equal(result.length, 0);
 };
 
+exports['add and retrieve simple item'] = function (test) {
+    items.add('foo');
+    var result = items.search('foo');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.ok(areEqual(result[0], { value: 'foo' }));
+};
