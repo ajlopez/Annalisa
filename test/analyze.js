88a6a2250140253a13cd0bffffb8ffd568a656c7
ajlopez <webmaster@ajlopez.com>
2014-02-23 09:02:27 -0300

    analyze compound terms

---
+++ test/analyze.js
@@ -0,0 +1,21 @@
+﻿
+var anna = require('..');
+
+function areEqual(obj1, obj2) {
+    if (Object.keys(obj1).length != Object.keys(obj2).length)
+        return false;
+        
+    for (var n in obj1)
+        if (obj1[n] != obj2[n])
+            return false;
+            
+    return true;
+}
+
+exports['analyze cheese'] = function (test) {
+    var data = { category: 'dairy', type: 'cheese' };
+    anna.define('cheese', { category: 'dairy', type: 'cheese' });
+    test.ok(areEqual(anna.analyze('foo'), { }));
+    test.ok(areEqual(anna.analyze('Blue Cheese'), data));
+    test.ok(areEqual(anna.analyze('Cheese 800gr'), data));
+}
