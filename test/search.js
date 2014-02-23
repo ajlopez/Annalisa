d1769abccca11dd422b80451b3a870a8b84d9a72
ajlopez <webmaster@ajlopez.com>
2014-02-23 11:27:39 -0300

    search first test

---
+++ test/search.js
@@ -0,0 +1,11 @@
+﻿
+var items = require('../lib/items');
+
+exports['retrieve nothing'] = function (test) {
+    var result = items.search('foo');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 0);
+};
+
