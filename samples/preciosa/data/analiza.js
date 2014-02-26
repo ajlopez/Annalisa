5dfbf50f4529b4a5f3c9957a190f9cc5bd4a72f9
ajlopez <webmaster@ajlopez.com>
2014-02-25 21:03:21 -0300

    more preciosa analiza use case

--- samples/preciosa/data/analiza.js
+++ samples/preciosa/data/analiza.js
@@ -8,6 +8,7 @@ anna.define('sobre', { contenedor: 'sobre' });
 
 anna.define(['pack', anna.Integer], { contenedor: 'pack', numero: '${Integer}' });
 anna.define([anna.Integer, 'lt'], { tamanio: '${Integer}', unidad: 'litro' });
+anna.define([anna.Integer, 'ml'], { tamanio: '${Integer}', unidad: 'mililitro' });
 anna.define([anna.Integer, 'cc'], { tamanio: '${Integer}', unidad: 'centimetros cubicos' });
 anna.define([anna.Integer, 'gr'], { peso: '${Integer}', unidad: 'gramo' });
 anna.define([anna.Integer, 'kg'], { peso: '${Integer}', unidad: 'kilogramo' });
@@ -16,13 +17,39 @@ var categorias = require('./categorias.json');
 var productos = require('./productos.json');
 
 categorias.forEach(function (categoria) {
+    defineCategoria(categoria);
+});
+
+function defineCategoria(categoria) {
+    var posy = categoria.indexOf(" y ");
+    
+    if (posy > 0) {
+        defineCategoria(categoria.substring(0, posy));
+        defineCategoria(categoria.substring(posy + 3));
+        return;
+    }
+    
+    var posbar = categoria.indexOf("/");
+    
+    if (posbar > 0) {
+        defineCategoria(categoria.substring(0, posbar));
+        defineCategoria(categoria.substring(posbar + 1));
+        return;
+    }
+
+    categoria = categoria.trim();
+    
     var ncategoria = categoria;
     
-    if (categoria.length && categoria[categoria.length - 1] == 's')
-        ncategoria = categoria.substring(0, categoria.length - 1);
+    ncategoria = ncategoria.replace(/nes /g, "n");
+    ncategoria = ncategoria.replace(/des /g, "d");
+    ncategoria = ncategoria.replace(/nes$/g, "n");
+    ncategoria = ncategoria.replace(/des$/g, "d");
+    ncategoria = ncategoria.replace(/s /g, "");
+    ncategoria = ncategoria.replace(/s$/g, "");
         
     anna.define(ncategoria, { categoria: categoria });
-});
+};
 
 productos.forEach(function (producto) {
     console.log(producto);
