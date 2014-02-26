f179efb6780dfe964fea23459391b087a69e8b35
ajlopez <webmaster@ajlopez.com>
2014-02-26 19:14:50 -0300

    Load marcas, fabricantes, first test

--- samples/preciosa/preciosa.js
+++ samples/preciosa/preciosa.js
@@ -1,10 +1,47 @@
 ﻿
 var anna = require('../..');
 
+var marcas = [];
+var fabricantes = [];
+
 function initialize() {
     anna.clear();
 }
 
+function loadMarcasFabricantes() {
+    var data = require('./fixtures/marcas.json');
+    
+    data.forEach(function (row) {
+        if (row.model == 'precios.empresafabricante')
+            addFabricante(row);
+        else if (row.model == 'precios.marca')
+            addMarca(row);
+    });
+}
+
+function addFabricante(row) {
+    var item = {
+        id: row.pk,
+        nombre: row.fields.nombre,
+        logo: row.fields.logo
+    };
+    
+    fabricantes[item.id] = item;
+}
+
+function addMarca(row) {
+    var item = {
+        id: row.pk,
+        nombre: row.fields.nombre,
+        fabricanteid: row.fields.fabricante
+    };
+    
+    marcas[item.id] = item;
+}
+
 module.exports = {
-    initialize: initialize
+    initialize: initialize,
+    loadMarcasFabricantes: loadMarcasFabricantes,
+    getFabricante: function (id) { return fabricantes[id]; },
+    getMarca: function (id) { return marcas[id]; }
 }
