d1995e99ca548afb3d38cecb0c2afca445d67f97
ajlopez <webmaster@ajlopez.com>
2014-03-03 16:56:33 -0300

    get remote brands from preciosa, spike

---
+++ samples/preciosa/data/getmarcas1.js
@@ -0,0 +1,20 @@
+
+var http = require('http');
+
+var url = 'http://preciosdeargentina.com.ar/api/v1/marcas/?page=1&format=json&page_size=100';
+
+http.get(url, function(res) {
+    var body = '';
+
+    res.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    res.on('end', function() {
+        console.log(body);
+        var fbResponse = JSON.parse(body);
+        console.dir(fbResponse);
+    });
+}).on('error', function(e) {
+      console.log("Got error: ", e);
+});
